aa2dba95095e4b63bca79f89fe41bc50d54a55ee
yayiekim <kimberly_abacco@live.com>
2016-03-04 20:57:17 +0800

    ok

---
+++ ClinicOne/ClinicOne/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClinicOne.Startup))]
+namespace ClinicOne
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
