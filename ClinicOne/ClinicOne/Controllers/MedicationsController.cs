242c2dd91f92ea9a003fa9f172959fb9e5ea5246
yayiekim <kimberly_abacco@live.com>
2016-03-07 21:25:31 +0800

    r

--- ClinicOne/ClinicOne/Controllers/MedicationsController.cs
+++ ClinicOne/ClinicOne/Controllers/MedicationsController.cs
@@ -1,17 +1,162 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using ClinicOne.Models;
+using System.Data.Entity;
 
 namespace ClinicOne.Controllers
 {
     public class MedicationsController : Controller
     {
+        ClinicOneEntities db = new ClinicOneEntities();
+
+
         // GET: Medications
         public ActionResult Index()
         {
             return View();
         }
+
+        public async Task<JsonResult> geMedicationCategories()
+        {
+            List<MedicationCategoryModel> thelist = new List<MedicationCategoryModel>();
+
+            var res = await db.DrugsCategories.ToListAsync();
+
+            foreach (var x in res)
+            {
+                MedicationCategoryModel model = new MedicationCategoryModel()
+                {
+                    Id = x.Id,
+                    CategoryName = x.Name
+
+                };
+
+                thelist.Add(model);
+            }
+
+            return Json(thelist, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> addMedicationCategory(MedicationCategoryModel category)
+        {
+            DrugsCategory model = new DrugsCategory()
+            {
+                Name = category.CategoryName
+
+            };
+
+            db.DrugsCategories.Add(model);
+            await db.SaveChangesAsync();
+
+            return Json(model.Id, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> editMedicationCategory(MedicationCategoryModel category)
+        {
+            var res = await db.DrugsCategories.FindAsync(category.Id);
+
+            res.Name = category.CategoryName;
+
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> deleteMedicationCategory(Guid id)
+        {
+            var res = await db.DrugsCategories.FindAsync(id);
+
+            db.DrugsCategories.Remove(res);
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+
+        //Drugs
+        public async Task<JsonResult> getMedications()
+        {
+            List<Drug> thelist = new List<Drug>();
+
+            var res = await db.Drugs.ToListAsync();
+
+            foreach (var x in res)
+            {
+
+                Drug model = new Drug() {
+
+                    Id = x.Id,
+                    MedicineName = x.MedicineName,
+                    Description = x.Description,
+                    Dosage = x.Dosage,
+                    Code = x.Code,
+                    Amount = x.Amount,
+                    DrugCatergoryId = x.DrugCatergoryId
+
+                };
+
+                thelist.Add(model);
+            }
+            
+            return Json(thelist, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> addMedication(MedicationModel medication)
+        {
+            Drug model = new Drug()
+            {
+                MedicineName = medication.MedicationName,
+                Amount = medication.Amount,
+                Code = medication.Code,
+                Description = medication.Description,
+                Dosage = medication.Dosage,
+                DrugCatergoryId = medication.CategoryId
+                
+            };
+
+            db.Drugs.Add(model);
+            await db.SaveChangesAsync();
+
+            return Json(model.Id, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> editMedication(MedicationModel medication)
+        {
+            var res = await db.Drugs.FindAsync();
+
+            res.MedicineName = medication.MedicationName;
+            res.Amount = medication.Amount;
+            res.Code = medication.Code;
+            res.Description = medication.Description;
+            res.Dosage = medication.Dosage;
+            res.DrugCatergoryId = medication.CategoryId;
+
+            await db.SaveChangesAsync();
+            
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> deleteMedication(Drug medication)
+        {
+            var res = await db.Drugs.FindAsync(medication.Id);
+
+            db.Drugs.Remove(res);
+            await db.SaveChangesAsync();
+
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+
+
     }
+
+
 }
+
+
+
---
+++ ClinicOne/ClinicOne/Models/MedicationModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicOne.Models
+{
+    public class MedicationModel
+    {
+        public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
+        public string MedicationName { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public string Dosage { get; set; }
+              
+    }
+
+    public class MedicationCategoryModel
+    {
+        public Guid Id { get; set; }
+        public string CategoryName { get; set; }
+
+
+
+    }
+
+
+
+}
