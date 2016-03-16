38c52630f77fb00dae8f47834380d0801f147fb7
yayiekim <kimberly_abacco@live.com>
2016-03-16 20:41:41 +0800

    consultation

--- ClinicOne/ClinicOne/Controllers/ConsultationController.cs
+++ ClinicOne/ClinicOne/Controllers/ConsultationController.cs
@@ -170,7 +170,248 @@ namespace ClinicOne.Controllers
 
         }
 
-        
+        public async Task<JsonResult> EditConsultation(ConsultationModel consultaion)
+        {
+            var res = await db.Consultations.FindAsync(consultaion.Id);
+
+            res.TransactionDate = consultaion.TransactonDate;
+
+          
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+
+        }
+
+        public async Task<JsonResult> DeleteConsultation(Guid id)
+        {
+            var res = await db.Consultations.FindAsync(id);
+
+
+            db.Consultations.Remove(res);
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+
+        }
+
+        public async Task<JsonResult> AddDiagnosis(ConsultationDiagnosisModel diagnosis)
+        {
+            ConsultationsDiagnosi model = new ConsultationsDiagnosi()
+            {
+                ConsultationId = diagnosis.ConsultationId,
+                Remarks = diagnosis.Remarks,
+                Diagnosis = diagnosis.Diagnosis
+
+
+            };
+
+            db.ConsultationsDiagnosis.Add(model);
+            await db.SaveChangesAsync();
+
+            return Json(model.Id, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> EditDiagnosis(ConsultationDiagnosisModel diagnosis)
+        {
+
+            var res = await db.ConsultationsDiagnosis.FindAsync(diagnosis.Id);
+
+            res.Remarks = diagnosis.Remarks;
+            res.Diagnosis = diagnosis.Diagnosis;
+
+
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> DeleteDiagnosis(Guid id)
+        {
+
+            var res = await db.ConsultationsDiagnosis.FindAsync(id);
+
+            db.ConsultationsDiagnosis.Remove(res);
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> AddRecord(PatientsRecord record)
+        {
+            PatientsRecord model = new PatientsRecord()
+            {
+                ConsultationId = record.ConsultationId,
+                RecordType = record.RecordType,
+                RecordValue = record.RecordValue
+
+            };
+
+            db.PatientsRecords.Add(model);
+            await db.SaveChangesAsync();
+
+            return Json(model.Id, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> EditRecord(RecordTypeModel record)
+        {
+
+            var res = await db.PatientsRecords.FindAsync(record.Id);
+
+            res.RecordType = record.RecordTypeName;
+            res.RecordValue = record.RecordTypeName;
+
+
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> DeleteRecord(Guid id)
+        {
+
+            var res = await db.PatientsRecords.FindAsync(id);
+
+            db.PatientsRecords.Remove(res);
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> AddPrescribeMed(PatientPrescribeMedicationModel medication)
+        {
+            PrescribedMedication model = new PrescribedMedication()
+            {
+                ConsultationId = medication.ConsultationId,
+                Medication = medication.Medication,
+                Quantity = medication.Quantity,
+                Remarks = medication.Remarks,
+                Amount = medication.Amount
+
+            };
+
+            db.PrescribedMedications.Add(model);
+            await db.SaveChangesAsync();
+
+            return Json(model.Id, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> EditPrescribeMed(PatientPrescribeMedicationModel medication)
+        {
+
+            var res = await db.PrescribedMedications.FindAsync(medication.Id);
+
+            res.Medication = medication.Medication;
+            res.Quantity = medication.Quantity;
+            res.Remarks = medication.Remarks;
+            res.Amount = medication.Amount;
+
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> DeletePrescribeMed(Guid id)
+        {
+
+            var res = await db.PrescribedMedications.FindAsync(id);
+
+            db.PrescribedMedications.Remove(res);
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> AddService(PatientOtherServiceModel service)
+        {
+            ConsultationsOtherService model = new ConsultationsOtherService()
+            {
+                ConsultationId = service.ConsultationId,
+                Description = service.Description,
+                Remarks = service.Remarks,
+                Amount = service.Amount
+
+
+            };
+
+            db.ConsultationsOtherServices.Add(model);
+            await db.SaveChangesAsync();
+
+            return Json(model.Id, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> EditService(PatientOtherServiceModel service)
+        {
+
+            var res = await db.ConsultationsOtherServices.FindAsync(service.Id);
+
+            res.Description = service.Description;
+            res.Remarks = service.Remarks;
+            res.Amount = service.Amount;
+
+
+
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> DeleteService(Guid id)
+        {
+
+            var res = await db.ConsultationsOtherServices.FindAsync(id);
+
+            db.ConsultationsOtherServices.Remove(res);
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> AddLab(PatientLabModel lab)
+        {
+            LabResult model = new LabResult()
+            {
+                ConsultationId = lab.ConsultationId,
+                Remarks = lab.Remarks,
+                RecordType = lab.RecordTypeName,
+                RecordValue =lab.RecordValue
+
+
+            };
+
+            db.LabResults.Add(model);
+            await db.SaveChangesAsync();
+
+            return Json(model.Id, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> EditLab(PatientLabModel lab)
+        {
+
+            var res = await db.LabResults.FindAsync(lab.Id);
+
+            res.RecordValue = lab.RecordValue;
+            res.RecordType = lab.RecordTypeName;
+            res.Remarks = lab.Remarks;
+
+
+
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<JsonResult> DeleteLab(Guid id)
+        {
+
+            var res = await db.LabResults.FindAsync(id);
+
+            db.LabResults.Remove(res);
+            await db.SaveChangesAsync();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
 
     }
 }
