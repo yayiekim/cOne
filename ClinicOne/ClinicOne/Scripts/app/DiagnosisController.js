f2410b9600346ac30bb97ddd8694ccfc3ceef5a1
bellcross11 <thousandsuns@live.com.ph>
2016-03-24 20:52:11 +0800

    aaaaaaa

---
+++ ClinicOne/ClinicOne/Scripts/app/DiagnosisController.js
@@ -0,0 +1,136 @@
+﻿var diagnosisController = angular.module('diagnosisController', []);
+
+
+
+diagnosisController.controller('diagnosisCtrl', function ($scope, $http) {
+
+    $scope.diagnosisList = [];
+    $scope.diagnosisListDisplay = [].concat($scope.diagnosisList);
+
+    $scope.submitType;
+
+    $scope.diagnosisCategoryList = [];
+
+
+    $scope.diagnosis = {
+
+        'Id': '',
+        'CategoryId': '',
+        'CategoryName': '',
+        'DiagnosisName': '',
+        'Description': ''
+    };
+
+    //GET DIAGNOSIS
+
+    $http({
+        method: 'GET',
+        url: '/Diagnosis/getDiagnosis'
+    }).success(function (data) {
+
+        $scope.diagnosisList = data;
+    });
+
+    //ADD DIAGNOSIS
+
+    $scope.showAddModal = function () {
+
+        $('#addModal').modal('toggle');
+
+        $scope.getDiagnosisCategory();
+
+        $scope.submitType = 'add';
+    }
+
+    //FOR DROPDOWNLIST DIAGNOSIS CATEGORY
+
+    $scope.getDiagnosisCategory = function () {
+
+        $http({
+
+            method: 'GET',
+            url: '/Diagnosis/getDiagnosisCategories'
+        }).success(function (data) {
+
+            $scope.diagnosisCategoryList = data;
+        });
+    }
+
+
+    //UPDATE DIAGNOSIS
+
+    $scope.updateDiagnosis = function () {
+
+        if ($scope.submitType == 'add') {
+
+            $http({
+                method: 'POST',
+                url: '/Diagnosis/addDiagnosis',
+                data: $.param({ diagnosis: $scope.diagnosis }),
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+
+            }).success(function (data) {
+
+
+                $scope.diagnosisList.push(data);
+
+            });
+        }
+        else {
+
+            $http({
+                method: 'POST',
+                url: '/Diagnosis/editDiagsosis',
+                data: $.param({ diagnosis: $scope.diagnosis }),
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+
+            }).success(function (data) {
+
+                $scope.diagnosisList.push($scope.diagnosis);
+
+            });
+        }
+    }
+
+
+    //DELETE DIAGNOSIS
+
+    $scope.showDeleteModal = function (row) {
+
+        $scope.diagnosis = row;
+
+        $('#deleteModal').modal('toggle');
+    }
+
+    $scope.deleteDiagnosis = function () {
+
+        $http({
+            method: 'GET',
+            url: '/Diagnosis/deleteDiagnosis?id=' + $scope.diagnosis.Id + ''
+
+        }).success(function (data) {
+
+            if (data == 'ok') {
+
+                var index = $scope.diagnosisList.indexOf($scope.diagnosis);
+
+                $scope.diagnosisList.splice(index, 1)
+            }
+        });
+    }
+
+
+    //EDIT DIAGNOSIS
+
+    $scope.showEditModal = function (row) {
+
+        $('#addModal').modal('toggle');
+
+        $scope.diagnosis = row;
+
+        $scope.submitType = 'edit';
+
+        $scope.getDiagnosisCategory();
+    }
+
+});
--- ClinicOne/ClinicOne/Scripts/app/RecordTypeController.js
+++ ClinicOne/ClinicOne/Scripts/app/RecordTypeController.js
@@ -1,1 +1,152 @@
-﻿
+﻿var recordTypeController = angular.module('recordTypeController', []);
+
+
+
+recordTypeController.controller('recordTypeCtrl', function ($scope, $http) {
+
+    $scope.recordTypesList = [];
+    $scope.recordTypesListDisplay = [].concat($scope.recordTypesList);
+
+    $scope.recordTypeCategoryList = [];
+    $scope.valueTypeList = [];
+
+
+    $scope.submitType;
+
+
+    //FOR DROPDOWN LIST RECORD CATEGORIES
+
+    $scope.getRecordTypeCategories = function () {
+
+        $http({
+            method: 'GET',
+            url: '/RecordTypes/geRecordCategories/'
+        }).success(function (data) {
+
+            $scope.recordTypeCategoryList = data;
+
+        }, function errorCallBack(data) {
+
+        });
+    }
+
+
+    //FOR DROPDOWN LIST VALUE TYPES CATEGORIES
+
+    $http({
+        method: 'GET',
+        url: '/RecordTypes/getValueTypes/'
+    }).success(function (data) {
+
+        $scope.valueTypeList = data;
+    });
+
+
+    //GET RECORDTYPES
+
+    $http({
+        method: 'GET',
+        url: '/RecordTypes/geRecordTypes/'
+    }).success(function (data) {
+
+        $scope.recordTypesList = data;
+    });
+
+
+    //ADD RECORDTYPES
+
+    $scope.recordTypes = {
+
+        'Id': '',
+        'RecordTypeName': '',
+        'RecordTypeCategoryId': '',
+        'RecordTypeCategoryName': '',
+        'ValueTypeId': '',
+        'ValueTypeName': ''
+    };
+
+    $scope.showAddModal = function () {
+
+        $('#addModal').modal('toggle');
+
+        $scope.getRecordTypeCategories();
+
+        $scope.submitType = 'add';
+    };
+
+
+    $scope.updateRecordType = function () {
+
+        if ($scope.submitType == 'add') {
+
+            $http({
+                method: 'POST',
+                url: '/RecordTypes/addRecordType/',
+                data: $.param({ recordType: $scope.recordTypes }),
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+
+            }).success(function (data) {
+
+                $scope.recordTypesList.push(data);
+            });
+        }
+        else {
+
+            $http({
+                method: 'POST',
+                url: '/RecordTypes/editRecordType/',
+                data: $.param({ recordType: $scope.recordTypes }),
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).success(function (data) {
+
+                $scope.recordTypesList.push($scope.recordTypes);
+            });
+
+        }
+
+
+    }
+
+
+    //DELETE RECORD TYPE
+
+    $scope.showDeleteModal = function (row) {
+
+        $scope.recordTypes = row;
+
+        $('#deleteModal').modal('toggle');
+    }
+
+
+    $scope.deleteRecordType = function () {
+
+        $http({
+            method: 'GET',
+            url: '/RecordTypes/deleteRecordType?id=' + $scope.recordTypes.Id + ''
+
+        }).success(function (data) {
+
+            if (data == 'ok') {
+
+                var index = $scope.recordTypesList.indexOf($scope.recordTypes);
+
+                $scope.recordTypesList.splice(index, 1)
+            }
+        });
+    }
+
+
+    //EDIT RECORD TYPE
+
+    $scope.showEditModal = function (row) {
+
+        $('#addModal').modal('toggle');
+
+        $scope.recordTypes = row;
+
+        $scope.submitType = 'edit';
+
+        $scope.getRecordTypeCategories();
+    }
+
+});
