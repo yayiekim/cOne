bc98f34f67f9ac60b3ed971c0c724cac07192071
bellcross11 <thousandsuns@live.com.ph>
2016-10-27 20:41:32 +0800

    fsfsdf

---
+++ ClinicOne/ClinicOne/Controllers/LabTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClinicOne.Controllers
+{
+    public class LabTestController : Controller
+    {
+        // GET: LabTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ClinicOne/ClinicOne/Scripts/app/LabRecordCategoriesController.js
@@ -0,0 +1,130 @@
+﻿var labRecordCategoriesController = angular.module('labRecordCategoriesController', []);
+
+
+labRecordCategoriesController.controller('labRecordCategoriesCtrl', function ($scope, $http) {
+    
+        $scope.labRecordCategoryList = [];
+        $scope.labRecordCategoryListDisplay = [].concat($scope.labRecordCategoryList);
+
+        $scope.submitType;
+
+        $scope.disableInput = false;
+
+        $http({
+            method: 'GET',
+            url: '/RecordTypes/getRecordCategories?ClassId=2'
+
+        }).success(function (data) {
+            $scope.labRecordCategoryList = data;
+
+        });
+
+        //ADD CATEGORY
+
+        $scope.Category = {
+            Id: '',
+            Category: '',
+            CategoryClassId: ''
+        };
+
+        $scope.showAddModal = function (row) {
+
+            $scope.Category = row;
+
+            $('#addModal').modal('toggle');
+
+            $scope.submitType = 'add';
+
+            $('#modalTitle').text('New');
+
+        }
+
+        $scope.updateCategory = function () {
+
+            $scope.Category.CategoryClassId = 2;
+
+
+            if ($scope.submitType == 'add') {
+
+                $http({
+                    method: 'POST',
+                    url: '/RecordTypes/addRecordCategory/',
+                    data: $.param({ category: $scope.Category }),
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+
+                }).success(function (data) {
+
+                    $scope.Category.Id = data;
+                    $scope.labRecordCategoryList.push($scope.Category);
+                });
+            }
+            else {
+
+                $http({
+                    method: 'POST',
+                    url: '/RecordTypes/editRecordCategory',
+                    data: $.param({ category: $scope.Category }),
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                }).success(function (data) {
+
+                    if (data == 'ok') {
+
+                        $scope.labRecordCategoryList.push($scope.Category);
+                    }
+
+                });
+            }
+
+        }
+
+        //EDIT CATEGORY
+
+
+        $scope.showEditModal = function (row) {
+
+            $('#addModal').modal('toggle');
+
+            $scope.Category = row;
+
+            $scope.submitType = 'edit';
+
+            $('#modalTitle').text('Edit');
+        }
+
+
+        //DELETE CATEGORY
+
+
+        $scope.showDeleteModal = function (row) {
+
+            $scope.Category = row;
+
+            $('#deleteModal').modal('toggle');
+
+            $('#CategoryName').text();
+
+            $('#modalTitle').text('Delete');
+        }
+
+
+        $scope.deleteCategory = function () {
+
+
+            $http({
+                method: 'GET',
+                url: '/RecordTypes/delete?id=' + $scope.Category.Id + ''
+            }).success(function (data) {
+
+                if (data == "ok") {
+
+                    var index = $scope.labRecordCategoryList.indexOf($scope.Category);
+
+                    $scope.labRecordCategoryList.splice(index, 1);
+                }
+
+            }, function errorCallback(data) {
+
+            });
+        };
+
+});
